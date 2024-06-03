722a14ce446a7289ce19f14396266a5a48ea3c8b
emptybutton <88573504+emptybutton@users.noreply.github.com>
2024-06-03 15:31:55 +0700

    ref(`Users`): use `Connection` instead of `Session`

--- backend/src/auth/infrastructure/repos.py
+++ backend/src/auth/infrastructure/repos.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy import Connection, select
 
 from src.auth.application.ports import repos
 from src.auth.domain import entities, value_objects
@@ -9,8 +8,8 @@ from src.shared.infrastructure.db import models
 
 
 class Users(repos.Users):
-    def __init__(self, session: Session) -> None:
-        self.__session = session
+    def __init__(self, connection: Connection) -> None:
+        self.__connection = connection
 
     def add(self, user: entities.User) -> None:
         pass
@@ -22,7 +21,7 @@ class Users(repos.Users):
             select(models.User.id, models.User.name, models.User.password_hash)
             .where(models.User.name == username.text)
         )
-        row_user = self.__session.execute(query).first()
+        row_user = self.__connection.execute(query).first()
 
         if row_user is None:
             return None
@@ -40,5 +39,5 @@ class Users(repos.Users):
             .exists()
         )
 
-        result: bool = self.__session.execute(query).scalar()  # type: ignore[call-overload]
+        result: bool = self.__connection.execute(query).scalar()  # type: ignore[call-overload]
         return result
