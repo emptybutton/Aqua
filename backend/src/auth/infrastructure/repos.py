1082f78895c3a090ab24824058a245192389ae42
emptybutton <88573504+emptybutton@users.noreply.github.com>
2024-06-03 15:09:29 +0700

    ref(`Users`): request only necessary data

--- backend/src/auth/infrastructure/repos.py
+++ backend/src/auth/infrastructure/repos.py
@@ -3,13 +3,12 @@ from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
-from src.auth.domain import entities
-from src.auth.domain import value_objects
-from src.auth.infrastructure.converters import Entities
+from src.auth.application.ports import repos
+from src.auth.domain import entities, value_objects
 from src.shared.infrastructure.db import models
 
 
-class Users:
+class Users(repos.Users):
     def __init__(self, session: Session) -> None:
         self.__session = session
 
@@ -19,16 +18,27 @@ class Users:
     def get_by_name(
         self, username: value_objects.Username
     ) -> Optional[entities.User]:
-        stmt = select(models.User).where(models.User.name == username.text)
-        user_model = self.__session.scalars(stmt).first()
+        query = (
+            select(models.User.id, models.User.name, models.User.password_hash)
+            .where(models.User.name == username.text)
+        )
+        row_user = self.__session.execute(query).first()
 
-        return None if user_model is None else Entities.user_of(user_model)
+        if row_user is None:
+            return None
+
+        return entities.User(
+            id=row_user.id,
+            name=value_objects.Username(row_user.name),
+            password_hash=value_objects.PasswordHash(row_user.password_hash),
+        )
 
     def has_with_name(self, username: value_objects.Username) -> bool:
-        stmt = (
+        query = (
             select(models.User)
             .where(models.User.name == username.text)
             .exists()
         )
 
-        return self.__session.scalar(stmt).first()
+        result: bool = self.__session.execute(query).scalar()  # type: ignore[call-overload]
+        return result
