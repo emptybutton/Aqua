d9ee05d6e4b25599b71a9061b2be6328570a3acf
emptybutton <88573504+emptybutton@users.noreply.github.com>
2024-06-04 18:57:50 +0700

    chore(`entrypoint`): add registration logic

---
+++ backend/src/entrypoint/application/ports/actions.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Protocol, Optional, TypeVar
+
+from src.shared.application.ports.uows import UoW
+
+
+@dataclass(frozen=True)
+class AuthUserRegistrationDTO:
+    user_id: int
+
+
+UoWT_contra = TypeVar("UoWT_contra", bound=UoW[object], contravariant=True)
+
+
+class RegisterAuthUser(Protocol[UoWT_contra]):
+    @abstractmethod
+    def __call__(
+        self,
+        name: str,
+        password: str,
+        *,
+        uow: UoWT_contra,
+    ) -> AuthUserRegistrationDTO: ...
+
+
+class RegisterAquaUser(Protocol[UoWT_contra]):
+    @abstractmethod
+    def __call__(  # noqa: PLR0913
+        self,
+        auth_user_id: int,
+        water_balance_milliliters: Optional[int],
+        glass_milliliters: Optional[int],
+        weight_kilograms: Optional[int],
+        *,
+        uow: UoWT_contra,
+    ) -> object: ...
---
+++ backend/src/entrypoint/application/registration.py
@@ -0,0 +1,31 @@
+from typing import TypeVar, Optional
+
+from src.entrypoint.application.ports import actions
+from src.shared.application.ports.uows import UoW
+
+
+_UoWT = TypeVar("_UoWT", bound=UoW[object])
+
+
+async def register_user(  # noqa: PLR0913
+    name: str,
+    password: str,
+    water_balance_milliliters: Optional[int],
+    glass_milliliters: Optional[int],
+    weight_kilograms: Optional[int],
+    *,
+    uow: _UoWT,
+    register_auth_user: actions.RegisterAuthUser[_UoWT],
+    register_aqua_user: actions.RegisterAquaUser[_UoWT],
+) -> None:
+    async with uow as uow:
+        dto = register_auth_user(name, password, uow=uow)
+        auth_user_id = dto.user_id
+
+        register_aqua_user(
+            auth_user_id,
+            water_balance_milliliters,
+            glass_milliliters,
+            weight_kilograms,
+            uow=uow,
+        )
