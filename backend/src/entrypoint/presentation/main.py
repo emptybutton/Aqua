ff7f00a4eec189a8475f2fba6e4731d41382d701
emptybutton <88573504+emptybutton@users.noreply.github.com>
2024-06-07 16:15:39 +0700

    feat(`entrypoint`): add api launch

---
+++ backend/src/entrypoint/presentation/main.py
@@ -0,0 +1,23 @@
+from typing import Callable, Awaitable
+
+from fastapi import FastAPI, Request, Response
+
+from src.entrypoint.presentation.routes import router
+from src.entrypoint.presentation.error_messages import for_api
+
+
+app = FastAPI()
+
+
+@app.middleware("http")
+async def error_handler_middleware(
+    request: Request,
+    call_next: Callable[[Request], Awaitable[Response]],
+) -> Response:
+    try:
+        return await call_next(request)
+    except Exception as error:
+        raise for_api(error) from error
+
+
+app.include_router(router)
