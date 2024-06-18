3331a50e0f0eda9d6285a70252a0770767a0a5de
emptybutton <88573504+emptybutton@users.noreply.github.com>
2024-06-18 20:29:42 +0700

    fix(`__init__.py`): import all modules

--- backend/src/entrypoint/presentation/facade/controllers/__init__.py
+++ backend/src/entrypoint/presentation/facade/controllers/__init__.py
@@ -1,4 +1,6 @@
 from src.entrypoint.presentation.facade.controllers import (
     registration as registration,  # noqa: PLC0414
     writing as writing,  # noqa: PLC0414
+    day_reading as day_reading,  # noqa: PLC0414
+    day_record_reading as day_record_reading,  # noqa: PLC0414
 )
