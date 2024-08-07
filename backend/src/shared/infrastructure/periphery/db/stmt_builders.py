61c02f50e2c2415469eb34167b5ddf7e926ab31c
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 <88573504+emptybutton@users.noreply.github.com>
2024-08-07 16:28:18 +0700

    fix(`stmt_builders`): use correct types

--- backend/src/shared/infrastructure/periphery/db/stmt_builders.py
+++ backend/src/shared/infrastructure/periphery/db/stmt_builders.py
@@ -1,14 +1,18 @@
-from typing import Self
+from typing import Self, Any, TypeVar, Generic
 
-from sqlalchemy import select, Select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.expression import Select
+
+
+_AnyTupleT = TypeVar("_AnyTupleT", bound=tuple[Any, ...])
 
 
 class STMTBuilder:
     def __init__(self, *, is_in_trasaction: bool) -> None:
         self.__is_in_trasaction = is_in_trasaction
 
-    def select(self, *args: object) -> "SelectBuilder":
+    def select(self, *args: Any) -> "SelectBuilder[_AnyTupleT]":  # noqa: ANN401
         stmt = select(*args)
 
         if self.__is_in_trasaction:
@@ -21,10 +25,15 @@ class STMTBuilder:
         return cls(is_in_trasaction=session.is_active)
 
 
-class SelectBuilder:
-    def __init__(self, stmt: Select, *, is_in_trasaction: bool) -> None:
+class SelectBuilder(Generic[_AnyTupleT]):
+    def __init__(
+        self,
+        stmt: Select[_AnyTupleT],
+        *,
+        is_in_trasaction: bool,
+    ) -> None:
         self.__stmt = stmt
         self.__is_in_trasaction = is_in_trasaction
 
-    def build(self) -> Select:
+    def build(self) -> Select[_AnyTupleT]:
         return self.__stmt
