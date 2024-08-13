a9987c32dcc29f2539291f3c933ec83a3989b2d0
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 <88573504+emptybutton@users.noreply.github.com>
2024-08-13 17:03:35 +0700

    ref(`containers`): use the `providers` word

--- services/backend/src/auth/presentation/di/containers.py
+++ services/backend/src/auth/presentation/di/containers.py
@@ -1,18 +1,18 @@
 from dishka import make_async_container, make_container
 
-from auth.presentation.di import providers as auth_provieers
+from auth.presentation.di import providers as auth_providers
 from shared.presentation.di import providers as shared_providers
 
 
 async_container = make_async_container(
     shared_providers.PeripheryProvider(),
     shared_providers.TransactionProvider(),
-    auth_provieers.RepoProvider(),
-    auth_provieers.GeneratorProvider(),
-    auth_provieers.SerializerProvider(),
+    auth_providers.RepoProvider(),
+    auth_providers.GeneratorProvider(),
+    auth_providers.SerializerProvider(),
 )
 
 sync_container = make_container(
-    auth_provieers.GeneratorProvider(),
-    auth_provieers.SerializerProvider(),
+    auth_providers.GeneratorProvider(),
+    auth_providers.SerializerProvider(),
 )
