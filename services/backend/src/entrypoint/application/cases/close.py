52c2625640f9454ab79cc27565411f5af557000b
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 <88573504+emptybutton@users.noreply.github.com>
2024-08-13 19:53:42 +0700

    fix(`close`): use type arguments

--- services/backend/src/entrypoint/application/cases/close.py
+++ services/backend/src/entrypoint/application/cases/close.py
@@ -5,14 +5,12 @@ from shared.application.ports.transactions import Transaction
 
 
 _TransactionT = TypeVar("_TransactionT", bound=Transaction)
-_AuthT = TypeVar("_AuthT", bound=clients.auth.Auth[_TransactionT])  # type: ignore[valid-type]
-_AquaT = TypeVar("_AquaT", bound=clients.aqua.Aqua[_TransactionT])  # type: ignore[valid-type]
 
 
 async def perform(
     *,
-    auth: clients.auth.Auth,
-    aqua: clients.aqua.Aqua,
+    auth: clients.auth.Auth[_TransactionT],
+    aqua: clients.aqua.Aqua[_TransactionT],
 ) -> None:
     await auth.close()
     await aqua.close()
