81a1e140fa54e1f3c218574ad32c0701d5a9208a
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 <88573504+emptybutton@users.noreply.github.com>
2024-08-19 17:55:32 +0700

    feat(`aqua`): use logger

--- services/backend/src/aqua/application/cases/read_day.py
+++ services/backend/src/aqua/application/cases/read_day.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from datetime import date
 
 from aqua.domain import entities
-from aqua.application.ports import repos
+from aqua.application.ports import repos, loggers
 
 
 @dataclass(kw_only=True, frozen=True)
@@ -26,6 +26,7 @@ async def perform(
     users: repos.Users,
     days: repos.Days,
     records: repos.Records,
+    logger: loggers.Logger,
 ) -> Output:
     user = await users.find_with_id(user_id)
 
@@ -35,7 +36,13 @@ async def perform(
     day = await days.find_from(date_, user_id=user.id)
     found_records = await records.find_from(date_, user_id=user.id)
 
+    if day is None and len(found_records) != 0:
+        await logger.log_records_without_day(found_records)
+
+    if day is not None and len(found_records) == 0:
+        await logger.log_day_without_records(day)
+
     if day is None:
-        day = entities.Day(user_id=user.id, target=user.target)
+        day = entities.Day.empty_of(user, date_=date_)
 
     return Output(user=user, day=day, records=found_records)
--- services/backend/src/aqua/application/cases/read_user.py
+++ services/backend/src/aqua/application/cases/read_user.py
@@ -3,7 +3,7 @@ from datetime import datetime, UTC
 from uuid import UUID
 
 from aqua.domain import entities
-from aqua.application.ports import repos
+from aqua.application.ports import repos, loggers
 
 
 @dataclass(kw_only=True, frozen=True)
@@ -25,6 +25,7 @@ async def perform(
     users: repos.Users,
     days: repos.Days,
     records: repos.Records,
+    logger: loggers.Logger,
 ) -> Output:
     user = await users.find_with_id(user_id)
 
@@ -35,7 +36,13 @@ async def perform(
     day = await days.find_from(today, user_id=user.id)
     found_records = await records.find_from(today, user_id=user.id)
 
+    if day is None and len(found_records) != 0:
+        await logger.log_records_without_day(found_records)
+
+    if day is not None and len(found_records) == 0:
+        await logger.log_day_without_records(day)
+
     if day is None:
-        day = entities.Day(user_id=user.id, target=user.target)
+        day = entities.Day.empty_of(user, date_=today)
 
     return Output(user=user, day=day, records=found_records)
