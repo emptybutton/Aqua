cc25912144ed52216b99d76593111546220f714d
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 <88573504+emptybutton@users.noreply.github.com>
2024-08-21 11:12:33 +0700

    chore(`structlog`): configure two loggers (#8)

--- services/backend/src/shared/infrastructure/periphery/structlog.py
+++ services/backend/src/shared/infrastructure/periphery/structlog.py
@@ -1,4 +1,23 @@
 import structlog
 
 
-dev_logger = structlog.wrap_logger(structlog.PrintLogger())
+dev_logger = structlog.wrap_logger(
+    structlog.PrintLogger(),
+    processors=[
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.TimeStamper(fmt="%H:%M:%S.%f", utc=False),
+        structlog.dev.ConsoleRenderer(),
+    ]
+)
+
+prod_logger = structlog.wrap_logger(
+    structlog.PrintLogger(),
+    processors=[
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso", utc=True),
+        structlog.processors.dict_tracebacks,
+        structlog.processors.JSONRenderer(),
+    ]
+)
