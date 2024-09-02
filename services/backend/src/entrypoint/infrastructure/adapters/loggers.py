77af6ee23a68283d8f3572567a0d8010294cf1cc
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 <88573504+emptybutton@users.noreply.github.com>
2024-09-02 16:46:06 +0700

    fix(`loggers`): use correct module names

--- services/backend/src/entrypoint/infrastructure/adapters/loggers.py
+++ services/backend/src/entrypoint/infrastructure/adapters/loggers.py
@@ -6,60 +6,68 @@ from entrypoint.infrastructure.periphery import logs
 from shared.infrastructure.periphery.structlog import dev_logger, prod_logger
 
 
-class AquaFacadeDevLogger(loggers.AquaLogger[clients.AquaFacade]):
-    async def log_aqua_is_not_working(self, aqua: clients.AquaFacade) -> None:
+class AquaFacadeDevLogger(loggers.AquaLogger[clients.aqua.AquaFacade]):
+    async def log_aqua_is_not_working(
+        self, aqua: clients.aqua.AquaFacade
+    ) -> None:
         for error in aqua.errors:
             await dev_logger.aerror(
                 logs.aqua_is_not_working_log, exc_info=error
             )
 
     async def log_no_user_from_other_parts(
-        self, aqua: clients.AquaFacade, user_id: UUID
+        self, aqua: clients.aqua.AquaFacade, user_id: UUID
     ) -> None:
         await dev_logger.awarning(
             logs.no_user_in_aqua_from_other_parts_log, user_id=user_id
         )
 
 
-class AuthFacadeDevLogger(loggers.AuthLogger[clients.AuthFacade]):
-    async def log_auth_is_not_working(self, auth: clients.AuthFacade) -> None:
+class AuthFacadeDevLogger(loggers.AuthLogger[clients.auth.AuthFacade]):
+    async def log_auth_is_not_working(
+        self, auth: clients.auth.AuthFacade
+    ) -> None:
         for error in auth.errors:
             await dev_logger.aerror(
                 logs.auth_is_not_working_log, exc_info=error
             )
 
     async def log_no_user_from_other_parts(
-        self, auth: clients.AuthFacade, user_id: UUID
+        self, auth: clients.auth.AuthFacade, user_id: UUID
     ) -> None:
         await dev_logger.awarning(
             logs.no_user_in_auth_from_other_parts_log, user_id=user_id
         )
 
 
-class AquaFacadeProdLogger(loggers.AquaLogger[clients.AquaFacade]):
-    async def log_aqua_is_not_working(self, aqua: clients.AquaFacade) -> None:
+class AquaFacadeProdLogger(loggers.AquaLogger[clients.aqua.AquaFacade]):
+    async def log_aqua_is_not_working(
+        self, aqua: clients.aqua.AquaFacade
+    ) -> None:
         for error in aqua.errors:
             await dev_logger.aerror(
                 logs.aqua_is_not_working_log, exc_info=error
             )
 
     async def log_no_user_from_other_parts(
-        self, aqua: clients.AquaFacade, user_id: UUID
+        self, aqua: clients.aqua.AquaFacade, user_id: UUID
     ) -> None:
         await dev_logger.awarning(
             logs.no_user_in_aqua_from_other_parts_log, user_id=user_id
         )
 
 
-class AuthFacadeProdLogger(loggers.AuthLogger[clients.AuthFacade]):
-    async def log_auth_is_not_working(self, auth: clients.AuthFacade) -> None:
+class AuthFacadeProdLogger(loggers.AuthLogger[clients.auth.AuthFacade]):
+    async def log_auth_is_not_working(
+        self, auth: clients.auth.AuthFacade
+    ) -> None:
         for error in auth.errors:
             await prod_logger.aerror(
                 logs.auth_is_not_working_log, exc_info=error
             )
 
     async def log_no_user_from_other_parts(
-        self, auth: clients.AuthFacade, user_id: UUID
+        self, auth: clients.auth.AuthFacade, user_id: UUID
     ) -> None:
         await prod_logger.awarning(
             logs.no_user_in_auth_from_other_parts_log, user_id=user_id
