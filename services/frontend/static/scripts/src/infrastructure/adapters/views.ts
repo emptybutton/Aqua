fd3881a935944de73517f0c479d52556be79a956
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2024-09-09 21:17:32 +0700

    feat(`static`): add `login` layout (#21)

--- services/frontend/static/scripts/src/infrastructure/adapters/views.ts
+++ services/frontend/static/scripts/src/infrastructure/adapters/views.ts
@@ -1,4 +1,7 @@
 import * as views from "../../application/ports/views.js";
+import * as _username from "../../domain/value-objects/username.js";
+import * as _password from "../../domain/value-objects/password.js";
+import * as _credentials from "../../domain/value-objects/credentials.js";
 
 export const pageView: views.WindowView = {
     redrawToLogin(): void {
@@ -23,5 +26,61 @@ export class ValidationCSSView implements views.ValidationView {
 
     redrawNeutral(): void {
         this._element.className = this._classNameWhenNeutral;
+    }
+}
+
+export class LoginNotificationLayoutView implements views.LoginNotificationView {
+    private _textElement: HTMLElement;
+
+    constructor(private _notificationElement: HTMLElement) {
+        if (_notificationElement.id !== "notification")
+            throw new Error("`_notificationElement` must have id = \"notification\"");
+        
+        let textElement = _notificationElement.querySelector("#text");
+
+        if (!(textElement instanceof HTMLElement))
+            throw new Error("`_notificationElement` must have `#text` subelement");
+
+        this._textElement = textElement;
+    }
+
+    redrawInvisible(): void {
+        this._notificationElement.style.display = "none";
+        this._textElement.innerText = "";
+    }
+
+    redrawNoUserWithUsername(_: _username.AnyUsername): void {
+        this._notificationElement.className = "bad-notification";
+        this._textElement.innerText = "Пользователя с таким именем нет";
+    }
+
+    redrawInvalidCredentials(_: _credentials.Credentials): void {
+        this._notificationElement.className = "bad-notification";
+        this._textElement.innerText = "Неправильный пароль";
+    }
+
+    redrawLastTimeThereWasNoUserNamed(_: _username.AnyUsername): void {
+        this._notificationElement.className = "neutral-notification";
+        this._textElement.innerText = "В прошлых попытках пользователя с таким именем не было";
+    }
+
+    redrawLastTimeThereWasNoUserWithCredentials(_: _credentials.Credentials): void {
+        this._notificationElement.className = "neutral-notification";
+        this._textElement.innerText = "В прошлых попытках такой пароль не подошёл";
+    }
+
+    redrawInvalidUsername(_: _username.AnyUsername): void {
+        this._notificationElement.className = "bad-notification";
+        this._textElement.innerText = "Не может быть пользователя с таким именем";
+    }
+
+    redrawInvalidPassword(_: _password.Password): void {
+        this._notificationElement.className = "bad-notification";
+        this._textElement.innerText = "Не может быть пользователя с таким паролем";
+    }
+
+    redrawTryAgainLater(): void {
+        this._notificationElement.className = "bad-notification";
+        this._textElement.innerText = "Что то пошло не по плану, попробуйте когда нибудь потом!";
     }
 }
