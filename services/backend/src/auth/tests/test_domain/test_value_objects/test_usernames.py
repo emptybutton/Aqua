28cf1c9e864162763013afc8027eb916c81265ca
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2024-10-15 19:06:54 +0700

    chore(`auth`): delete all `tests` (#30)

--- services/backend/src/auth/tests/test_domain/test_value_objects/test_usernames.py
+++
@@ -1,8 +0,0 @@
-from pytest import raises
-
-from auth.domain import value_objects as vos
-
-
-def test_creation_with_empty_text() -> None:
-    with raises(vos.Username.EmptyError):
-        vos.Username(text="")
