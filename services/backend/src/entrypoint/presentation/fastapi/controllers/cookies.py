3ae1079aac424fe62acedbb95cfff82074a5918c
Alexander Smolin <88573504+emptybutton@users.noreply.github.com>
2024-11-11 23:59:47 +0700

    fix(`cookies`): use `TypeAlias` instead of `type` (#50)

--- services/backend/src/entrypoint/presentation/fastapi/controllers/cookies.py
+++ services/backend/src/entrypoint/presentation/fastapi/controllers/cookies.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, TypeAlias
 
 from fastapi import Depends
 from fastapi.security import APIKeyCookie
@@ -6,20 +6,20 @@ from fastapi.security import APIKeyCookie
 from entrypoint.presentation.fastapi.views import cookies
 
 
-_session_cookie_scheme = APIKeyCookie(
+session_cookie_scheme = APIKeyCookie(
     name=cookies.SessionCookie.name,
     scheme_name="Session id cookie",
     description="Scheme for authentication via web sessions.",
 )
-type session_id_cookie = Annotated[str, Depends(_session_cookie_scheme)]
+session_id_cookie: TypeAlias = Annotated[str, Depends(session_cookie_scheme)]
 
 
-_optional_session_cookie_scheme = APIKeyCookie(
+optional_session_cookie_scheme = APIKeyCookie(
     name=cookies.SessionCookie.name,
     scheme_name="Optional session id cookie",
     description="Scheme for possible authentication via web sessions.",
     auto_error=False,
 )
-type optional_session_id_cookie = Annotated[
-    str | None, Depends(_optional_session_cookie_scheme)
+optional_session_id_cookie: TypeAlias = Annotated[
+    str | None, Depends(optional_session_cookie_scheme)
 ]
